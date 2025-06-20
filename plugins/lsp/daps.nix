50b27abd9af529e11e9ea72092da4722970d28c7
M K Mukesh Kannan <mukeshkannan311@gmail.com>
2025-06-20 13:08:10 +0530

    fix: codelldb (works but not fixed)

--- plugins/lsp/daps.nix
+++ plugins/lsp/daps.nix
@@ -2,8 +2,34 @@
 {
   extraPlugins = [
     pkgs.vimPlugins."nvim-dap"
-    pkgs.vimPlugins."nvim-dap-ui"
   ];
+
+    plugins.dap-ui = {
+      enable = true;
+      settings = {
+        icons = {
+          expanded = "▾";
+          collapsed = "▸";
+          current_frame = "*";
+        };
+
+        controls = {
+          icons = {
+            pause = "⏸";
+            play = "▶";
+            step_into = "⏎";
+            step_over = "⏭";
+            step_out = "⏮";
+            step_back = "b";
+            run_last = "▶▶";
+            terminate = "⏹";
+            disconnect = "⏏";
+          };
+        };
+      };
+    };
+
+
   extraConfigLua = ''
     local dap, dapui = require("dap"), require("dapui")
 
@@ -22,27 +48,31 @@
       dapui.close()
     end
 
-    -- dap.adapters.lldb = {
-    --   type = 'executable',
-    --   command = "${pkgs.lldb}/bin/lldb-dap", 
-    --   name = 'lldb',
-    -- }
 
-    -- dap.configurations.cpp = {
-    --   {
-    --     name = 'Launch',
-    --     type = 'lldb',
-    --     request = 'launch',
-    --     program = function()
-    --       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    --     end,
-    --     cwd = '$\{workspaceFolder}',
-    --     stopOnEntry = false,
-    --     args = {},
-    --   },
-    -- }
-    -- dap.configurations.c = dap.configurations.cpp
-    -- dap.configurations.rust = dap.configurations.cpp
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = "$\{port\}",
+      executable = {
+        command = "/Users/mukeshkannan/mk-nixvim/plugins/lsp/dap/codelldb/extension/adapter/codelldb", 
+        args = {"--port", "$\{port\}"},
+      }
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        host = "arm64",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+        cwd = '\$\{workspaceFolder\}',
+        stopOnEntry = false,
+      },
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.rust = dap.configurations.cpp
 
     vim.keymap.set('n', '<Leader>dt', function() dap.toggle_breakpoint() end)
     vim.keymap.set('n', '<Leader>db', function() dap.set_breakpoint() end)
@@ -51,12 +81,12 @@
     vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
     vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
     vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
-    vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpoint', { text='B', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
     vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
     vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
     vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
     vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
   '';
-  plugins.dap-lldb.enable = true;
-  plugins.dap-lldb.settings.codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+ # plugins.dap-lldb.enable = true;
+ # plugins.dap-lldb.settings.codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
 }
