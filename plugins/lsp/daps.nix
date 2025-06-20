0871f3abb9661dfbff1f3a6764f19415b6fd80f1
M K Mukesh Kannan <mukeshkannan311@gmail.com>
2025-06-20 11:00:00 +0530

    fix: codelldb (not-completely fixed)

--- plugins/lsp/daps.nix
+++ plugins/lsp/daps.nix
@@ -26,7 +26,7 @@
       type = 'server',
       port = "$\{port\}",
       executable = {
-        command = "/Users/mukeshkannan/my-nix-darwin/home/neovim/lsp/dap/codelldb/extension/adapter/codelldb", 
+        command = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb", 
         args = {"--port", "$\{port\}"},
       }
     }
@@ -49,6 +49,15 @@
     vim.keymap.set('n', '<Leader>dt', function() dap.toggle_breakpoint() end)
     vim.keymap.set('n', '<Leader>db', function() dap.set_breakpoint() end)
     vim.keymap.set('n', '<Leader>dc', function() dap.continue() end)
+
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+    vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+    vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+    vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
   '';
 
 }
